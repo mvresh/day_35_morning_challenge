92806650b4f642d75e6920df7ca506892f198952
Varun Resh Maddali <mvresh@gmail.com>
2019-11-21 12:16:18 +0530

    exercise done

--- bin/main.dart
+++ bin/main.dart
@@ -22,6 +22,117 @@
 //  stackCalc("3 DUP +") ➞ 6
 //  stackCalc("6 5 5 7 * - /") ➞ 5
 //  stackCalc("x y +") ➞ Invalid instruction: x
+import 'dart:collection';
+
+class Stack{
+
+}
+
+stackCalc(String series){
+  ListQueue stack = ListQueue.from(series.split(" "));
+
+  print(stack);
+  ListQueue<int> tempStack = ListQueue();
+  bool loop = true;
+  if(series == ''){
+    return 0;
+  }
+  while(loop){
+    if(stack.isNotEmpty){
+      int item;
+      try{
+        item = int.parse(stack.first);
+      }catch(e){
+//        print(e);
+      }
+      if(item is int){
+        int number = int.parse(stack.first);
+        tempStack.addFirst(number);
+        stack.removeFirst();
+      }
+      else if(stack.first == '+'){
+        if(tempStack.length >= 2){
+          stackOperation(stack, tempStack,'+');
+        }
+        else{
+          return 'Invalid instruction: Missing Number';
+        }
+      }
+      else if(stack.first == '-'){
+        if(tempStack.length >= 2){
+          stackOperation(stack, tempStack,'-');
+        }
+        else{
+          return 'Invalid instruction: Missing Number';
+        }
+      }
+      else if(stack.first == '*'){
+        if(tempStack.length >= 2){
+          stackOperation(stack, tempStack,'*');
+        }
+        else{
+          return 'Invalid instruction: Missing Number';
+        }
+      }
+      else if(stack.first == 'DUP'){
+        stack.removeFirst();
+        int a = tempStack.first;
+        tempStack.addFirst(a);
+      }
+      else if(stack.first == '/'){
+        if(tempStack.length >= 2){
+          stack.removeFirst();
+          int a = tempStack.first;
+          tempStack.removeFirst();
+          int b = tempStack.first;
+          tempStack.removeFirst();
+          if(b != 0){
+            tempStack.addFirst(a ~/ b);
+          }
+          else{
+            return 'error';
+          }
+        }
+        else{
+          return 'Invalid instruction: Missing Number';
+        }
+      }
+
+      else if(tempStack.length == 1){
+        loop = false;
+      }
+      else{
+        return 'Invalid instruction: ${stack.removeFirst()}';
+      }
+    }
+    else{
+      loop = false;
+    }
+  }
+  print(tempStack);
+  return tempStack.first;
+}
+
+void stackOperation(ListQueue stack, ListQueue<int> tempStack,String operation) {
+  stack.removeFirst();
+  int a = tempStack.first;
+  tempStack.removeFirst();
+  int b = tempStack.first;
+  tempStack.removeFirst();
+
+  if(operation == '+'){
+    tempStack.addFirst(a + b);
+  }
+  if(operation == '-'){
+    tempStack.addFirst(a - b);
+  }
+  if(operation == '*'){
+    tempStack.addFirst(a * b);
+  }
+}
+
 
 main() {
+  print(stackCalc("5 6 +"));
+  print(stackCalc("5 6 8 9 + -"));
 }
--- test/Day_35_Morning_Challenge_test.dart
+++ test/Day_35_Morning_Challenge_test.dart
@@ -1,7 +1,26 @@
 import 'package:test/test.dart';
+import '../bin/main.dart';
 
 void main() {
-  test('Test Case', (){
-
+  test('Test Case 1', (){
+    expect(stackCalc(''), equals(0));
   });
+
+  test('Test Case 2', (){
+    expect(stackCalc('5 -'), equals('Invalid instruction: Missing Number'));
+  });
+
+  test('Test Case 3', (){
+    expect(stackCalc("6 5 5 7 * - /"), equals(5));
+  });
+
+  test('Test Case 4', (){
+    expect(stackCalc(" "), equals('Invalid instruction: '));
+  });
+
+  test('Test Case 5', (){
+    expect(stackCalc("0 5 5 7 * - /"), equals('error'));
+  });
+
+
 }
